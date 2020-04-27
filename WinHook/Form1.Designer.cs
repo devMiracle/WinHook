7f7cb99b8d83d4ca18a8f886ef109a25fb7152d7
Miracle <rjcvjchzljv9@gmail.com>
2020-04-27 08:17:10 +0300

    1

--- WinHook/Form1.Designer.cs
+++ WinHook/Form1.Designer.cs
@@ -28,13 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PRESS F1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(87, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 73);
+            this.label2.TabIndex = 2;
+            this.label2.TextChanged += new System.EventHandler(this.label2_TextChanged);
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(292, 154);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TEST HOOK";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WinHook/Form1.cs
+++ WinHook/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Hook;
+using System.Threading;
 
 namespace WinHook
 {
@@ -15,6 +17,35 @@ namespace WinHook
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 0x70 клавиша F1
+            var _hook_F1 = new Hook.Hook(0x70);
+            _hook_F1.KeyPressed += new KeyPressEventHandler(_hook_F1_KeyPressed);
+            _hook_F1.SetHook();
+
+        }
+        void _hook_F1_KeyPressed(object sender, KeyPressEventArgs e) //Событие нажатия клавиш
+        {
+            label2.Text = "F1";
+        }
+
+       
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
         }
     }
 }
